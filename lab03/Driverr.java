c9db291ecb73e9a2f07d066d394d0b5921bd8123
krishaghodasara09 <krishaghodasara@gmail.com>
2026-07-24 14:36:46 +0530

    ptr2

---
+++ lab03/Driverr.java
@@ -0,0 +1,29 @@
+package lab03;
+
+public class Driverr {
+    public static void main(String[] args) {
+
+        Card[] cards = {
+            new Card("Ace", "Spades"),
+            new Card("Queen", "Hearts"),
+            new Card("King", "Diamonds"),
+            new Card("King", "Diamonds"),   // Duplicate
+            new Card("10", "Clubs")
+        };
+
+        boolean duplicateFound = false;
+
+        for (int i = 0; i < cards.length; i++) {
+            for (int j = 0; j < i; j++) {
+                if (cards[i].equals(cards[j])) {
+                    System.out.println("Duplicate found: " + cards[i]);
+                    duplicateFound = true;
+                    break;
+                }
+            }
+
+            if (duplicateFound)
+                break;
+        }
+    }
+}
