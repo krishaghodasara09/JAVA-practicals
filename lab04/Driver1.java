c218e42d9d624d7e1f6a409474e9f2fb218b0f4b
krishaghodasara09 <krishaghodasara@gmail.com>
2026-08-06 10:31:05 +0530

    Ptr4

---
+++ lab04/Driver1.java
@@ -0,0 +1,23 @@
+package lab04;
+
+public class Driver1 {
+    public static void main(String[] args) {
+        String[] passwords = {
+            "abc",
+            "PasssW",
+            "Abcd1234!",
+        };
+
+        for (String pwd : passwords) {
+
+            System.out.println("Password: " + pwd);
+            System.out.println("Length >= 8      : " + Passwordchecker.length(pwd));
+            System.out.println("Uppercase Letter : " + Passwordchecker.hasupper(pwd));
+            System.out.println("Contains Digit   : " + Passwordchecker.hasdigit(pwd));
+            System.out.println("Special Character: " + Passwordchecker.hasspecial(pwd));
+            System.out.println("Strength: " + Passwordchecker.strength(pwd));
+
+            System.out.println("----------------------------");
+        }
+    }
+}
---
+++ lab04/Passwordchecker.java
@@ -0,0 +1,36 @@
+package lab04;
+
+import java.util.*;
+public class Passwordchecker{
+    public static Boolean length(String pwd){
+        return pwd.length()>=8;
+    }
+    public static Boolean hasupper(String pwd){
+        return pwd.matches(".*[A-Z].*");
+    }
+    public static Boolean hasdigit(String pwd){
+        return pwd.matches(".*[0-9].*");
+    }
+    public static Boolean hasspecial(String pwd){
+        return pwd.matches(".*[^a-zA-Z0-9].*");
+    }
+    public static String strength(String pwd){
+        int count=0;
+        if(length(pwd))
+            count++;
+        if(hasupper(pwd))
+            count++;
+        if(hasdigit(pwd))
+            count++;
+        if(hasspecial(pwd))
+            count++;
+
+        if(count<=1)
+            return "Weak";
+        else if(count==4)
+            return "Medium";
+        else
+            return "Strong";
+    }
+}
+ 
